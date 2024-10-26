290b74e6209bcc244b5b454ebc3c3b020f692594
yizelc <cryizhel@gmail.com>
2024-10-25 18:19:00 -0600

    Agregando mi proyecto inicial

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: 'api-evaluacion2-r2d1-h0q89ljg5-yizelcs-projects.vercel.app',
 });
 
 export default api;
